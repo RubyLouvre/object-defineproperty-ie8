e658dee9db2f09bcab3b917cc2df4684993d08f8
RubyLouvre <1669866773@qq.com>
2018-07-05 16:31:01 +0800

    add

---
+++ index.js
@@ -0,0 +1,28 @@
+var origDefineProperty = Object.defineProperty;
+
+var arePropertyDescriptorsSupported = function() {
+  var obj = {};
+  try {
+    origDefineProperty(obj, "x", { enumerable: false, value: obj });
+    for (var _ in obj) {
+      return false;
+    }
+    return obj.x === obj;
+  } catch (e) {
+    /* this is IE 8. */
+    return false;
+  }
+};
+var supportsDescriptors =
+  origDefineProperty && arePropertyDescriptorsSupported();
+
+if (!supportsDescriptors) {
+  Object.defineProperty = function(a, b, c) {
+    //IE8支持修改元素节点的属性
+    if (origDefineProperty && a.nodeType == 1) {
+      return origDefineProperty(a, b, c);
+    } else {
+      a[b] = c.value;
+    }
+  };
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# object-defineproperty-ie8
+
+支持IE8的Object.defineProperty
+
+```shell
+npm i object-defineproperty-ie8
+```
